7b5ebbee859ff20ac8ab5daab91091211c529154
dianzrva <diana.nazapowa64@gmail.com>
2024-04-03 20:25:00 +0200

    hw 6

--- js/task-2.js
+++ js/task-2.js
@@ -1,74 +1,28 @@
 'use strict';
-// Напиши стрілочну функцію getUsersWithFriend(users, friendName) , яка прийматиме два параметра:
-
-// перший параметр users — масив об’єктів користувачів
-// другий параметр friendName — ім’я друга для пошуку.
-// Функція має повертати масив усіх користувачів із масиву users, у яких є друг з іменем friendName. Друзі кожного користувача зберігаються у властивості friends. Якщо користувачів, у яких є такий других немає, то функція має повернути порожній масив.
-
-const getUsersWithFriend = (users, friendName) => {
-    return users.filter(user => user.friends.includes(friendName));
-}
-
-// Поради:
-
-// Метод filter() можна використовувати для створення нового масиву з елементами, які задовольняють певну умову.
-// Використовуй метод includes() для перевірки, чи масив friends містить friendName.
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
-
-const allUsers = [
+{
+  let task = '2';
+  console.log(`----------------task-${task}--------------------`);
   {
-    name: "Moore Hensley",
-    friends: ["Sharron Pace"]
-  },
-  {
-    name: "Sharlene Bush",
-    friends: ["Briana Decker", "Sharron Pace"]
-  },
-  {
-    name: "Ross Vazquez",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
-  },
-  {
-    name: "Elma Head",
-    friends: ["Goldie Gentry", "Aisha Tran"]
-  },
-  {
-    name: "Carey Barr",
-    friends: ["Jordan Sampson", "Eddie Strong"]
-  },
-  {
-    name: "Blackburn Dotson",
-    friends: ["Jacklyn Lucas", "Linda Chapman"]
-  },
-  {
-    name: "Sheree Anthony",
-    friends: ["Goldie Gentry", "Briana Decker"]
+    class Storage {
+      #items = ['Nanitoids', 'Prolonger', 'Antigravitator'];
+      getItems() {
+        return this.#items;
+      } //— повертає масив поточних товарів у приватній властивості items.
+      addItem(newItem) {
+        this.#items.push(newItem);
+      } // — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
+      removeItem(itemToRemove) {
+        this.#items = this.#items.filter(function (item) {
+          return item !== itemToRemove;
+        });
+      } //— приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
+    }
+    const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+    console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+    storage.addItem('Droid');
+    console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+    storage.removeItem('Prolonger');
+    console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
   }
-];
-
-console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
-// [
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
-
-console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-// [
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
-
-console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+  console.log(`----------------task-${task}--------------------`);
+}
--- js/task-3.js
+++ js/task-3.js
@@ -1,89 +1,36 @@
 'use strict';
-// Напиши стрілочну функцію sortByDescendingFriendCount(users) , яка прийматиме один параметр users — масив об’єктів користувачів.
-
-// Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів (властивість friends).
-
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
-
-const sortByDescendingFriendCount = (users) => {
-    return allUsers.sort((a, b) => b.friends.length - a.friends.length);
-}
-
-
-console.log(
-  sortByDescendingFriendCount([
-    {
-      name: "Moore Hensley",
-      friends: ["Sharron Pace"],
-      gender: "male"
-    },
-    {
-      name: "Sharlene Bush",
-      friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female"
-    },
-    {
-      name: "Ross Vazquez",
-      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male"
-    },
-    {
-      name: "Elma Head",
-      friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female"
-    },
-    {
-      name: "Carey Barr",
-      friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male"
-    },
-    {
-      name: "Blackburn Dotson",
-      friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male"
-    },
-    {
-      name: "Sheree Anthony",
-      friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female"
+{
+  let task = '3';
+  console.log(`----------------task-${task}--------------------`);
+  {
+    class StringBuilder {
+      #value;
+      constructor(initialValue) {
+        this.#value = initialValue;
+      }
+      getValue() {
+        return this.#value;
+      } //— повертає поточне значення приватної властивості value.
+      padEnd(str) {
+        this.#value = this.#value + str;
+      } //— отримує параметр str(рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+      padStart(str) {
+        this.#value = str + this.#value;
+      } //— отримує параметр str(рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод.
+      padBoth(str) {
+        this.padStart(str);
+        this.padEnd(str);
+      } //— отримує параметр str(рядок) і додає його на початок і в кінець значення приватної властивості  value об'єкта, який викликає цей метод.
     }
-  ])
-);
-// [
-//   {
-//     name: "Ross Vazquez",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Carey Barr",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Moore Hensley",
-//     friends: ["Sharron Pace"],
-//     gender: "male"
-//   }
-// ]
+
+    const builder = new StringBuilder('.');
+    console.log(builder.getValue()); // "."
+    builder.padStart('^');
+    console.log(builder.getValue()); // "^."
+    builder.padEnd('^');
+    console.log(builder.getValue()); // "^.^"
+    builder.padBoth('=');
+    console.log(builder.getValue()); // "=^.^="
+  }
+  console.log(`----------------task-${task}--------------------`);
+}
